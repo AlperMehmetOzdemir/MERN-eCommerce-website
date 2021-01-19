6d9cbc9f3c0854214651167a6681cc15ec372736
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-01-19 22:06:20 +0300

    Setup react-bootstrap with lux theme from bootswatch. initial setup of  header, footer, rating components and homescreen page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// Screens
+import HomeScreen from "./screens/HomeScreen"
+
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
