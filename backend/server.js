cbea60c85696f056c4c91a60960f0d1f284df30e
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-01-20 11:45:32 +0300

    Connected the backend to mongoDb. Created user,product and order models using mongoose. Created a database seeder script for development purposes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDb from "./config/db.js";
+import products from "./data/products.js";
+
+dotenv.config();
+
+connectDb();
 
 const app = express();
 
@@ -13,9 +20,9 @@ app.get("/api/products", (req,res) => {
 
 app.get("/api/products/:id", (req,res) => {
   const product = products.find(p => p._id === req.params.id);
-  res.json(product)
+  res.json(product);
 })
 
+const PORT = process.env.PORT || 5000
 
-
-app.listen(5000, console.log("Server running on port 5000"));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
