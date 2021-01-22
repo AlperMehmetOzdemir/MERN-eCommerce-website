873bdefcea0799237fdf34273484d6eef38e597e
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-01-22 13:06:45 +0300

    Setup react-redux for products. Created a Loader and Message component using react bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import axios from "axios"
-import { Row, Col } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get("/api/products");
-
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
-     <h1>Latest Products</h1> 
-     <Row>
-      {products.map(product => (
-        <Col key={product._id} sm={12} md={6} lg={4}>
-          <Product product={product} />
-        </Col>
-      ))}
-     </Row>
-
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
