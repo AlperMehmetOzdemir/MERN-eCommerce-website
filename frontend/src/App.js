c1a570fd9a879a973e8ae3a3ef9b28ab78d3d2c9
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-01-23 10:13:23 +0300

    Setup store, reducers and actions for cart (add item). Major portion of cartscreen finished.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Header from "./components/Header";
 // Screens
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
+
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
         <Container>
           <Route path="/" component={HomeScreen} exact/>
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
