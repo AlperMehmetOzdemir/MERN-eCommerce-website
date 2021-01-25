ee24ccb5b2a894f0ca70a7a386376d9c7b0d52e2
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-01-25 17:46:07 +0300

    Added user authentication and registration in backend. Added password encryption with bcrypt. Added middleware to auto encrypt password on save for the User model

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// IMPORTS
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -6,23 +7,32 @@ import connectDb from "./config/db.js";
 // middleware
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-// routes
+// route imports
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js"
 
+// SETUP & MIDDLEWARE
 dotenv.config();
 
 connectDb();
 
 const app = express();
 
+app.use(express.json())
+
 // api test
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+// ROUTES
 // product routes
 app.use("/api/products", productRoutes);
+//user routes
+app.use("/api/users", userRoutes)
 
+
+// CUSTOM MIDDLEWARE
 // error middleware
 app.use(notFound);
 app.use(errorHandler);
